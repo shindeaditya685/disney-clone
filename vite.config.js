126c399ec9c3f00c564a876dbc8a670908413bb6
Aditya Shinde <82741617+shindeaditya685@users.noreply.github.com>
2024-03-09 10:27:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/31-disney-clone/",
+  base: "/disney-clone/",
   plugins: [react()],
 });
